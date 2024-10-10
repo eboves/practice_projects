f0e3d44d14a934fddf3b7f366b8608cef694ec99
Elvis Boves <elvisboves@Elviss-MBP.attlocal.net>
2024-10-10 11:42:36 -0400

    day 28: Pomodoro timer

--- day_28/pomodoro-start/main.py
+++ day_28/pomodoro-start/main.py
@@ -1,4 +1,4 @@
-
+import tkinter as tk
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -16,3 +16,15 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = tk.Tk()
+window.title("Pomodoro")
+window.config(padx=103, pady=50, bg=YELLOW)
+canvas = tk.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+bg_image = tk.PhotoImage(file="tomato.png")
+canvas.create_image(100,112,image=bg_image)
+canvas.pack()
+canvas.create_text(103,130, text="00:00", fill="white",font=(FONT_NAME,35,"bold"))
+
+
+
+window.mainloop()
